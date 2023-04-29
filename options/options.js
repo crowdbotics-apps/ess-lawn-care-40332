4dfed86d369ab49cf540f2b27dfa239b1705b6ff
Crowdbotics <team@crowdbotics.com>
2023-04-29 06:29:45 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ess-lawn-care-40332",
+  url: "https://ess-lawn-care-40332.botics.co",
+  api: "https://ess-lawn-care-40332.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
